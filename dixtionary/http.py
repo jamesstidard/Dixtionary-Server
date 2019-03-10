dca83826ef9bc669e83bbf1a2207d7ddfeeed6e4
James Stidard <james@stidard.com>
2019-03-10 17:20:09 +0000

    formatting

--- dixtionary/http.py
+++ dixtionary/http.py
@@ -7,9 +7,10 @@ from graphene import Schema
 async def graphql_handler(request: Request):
     query: str = request.raw_args.get('query')
     schema: Schema = request.app.schema
+
     result = schema.execute(query, context=request)
 
     if result.errors:
         raise ValueError(result.errors)
-
-    return json(result.data, indent=2)
+    else:
+        return json(result.data, indent=2)
